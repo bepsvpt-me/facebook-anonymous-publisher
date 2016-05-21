0dc81c77ec0c0c1065a059116bdd97e49c10b6f3
BePsvPT <freedom@csie.io>
2016-05-21 16:32:13 +0800

    Update languages

---
+++ app/Console/Commands/UpdateLang.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class UpdateLang extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lang:update {--lang=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+    protected $description = 'Update the language translation.';
+
+    /**
+     * @var Filesystem
+     */
+
+    protected $filesystem;
+
+    /**
+     * @var string
+     */
+
+    protected $srcPath;
+
+    /**
+     * UpdateLang constructor.
+     *
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        parent::__construct();
+
+        $this->filesystem = $filesystem;
+
+        $this->srcPath = file_build_path('vendor', 'caouecs', 'laravel-lang', 'src');
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $langs = $this->option('lang');
+
+        if (! is_null($langs) && $this->filesystem->isDirectory($this->srcPath($langs))) {
+            $langs = [$langs];
+        } else {
+            $langs = $this->filesystem->directories($this->srcPath());
+        }
+
+        foreach ($langs as $lang) {
+            $this->filesystem->copyDirectory(
+                $lang,
+                resource_path(file_build_path('lang', $this->filesystem->name($lang)))
+            );
+        }
+
+        $this->info('Language updated!');
+    }
+
+    /**
+     * Get the path to the base of the language src.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    protected function srcPath($path = '')
+    {
+        return base_path($this->srcPath.($path ? DIRECTORY_SEPARATOR.$path : $path));
+    }
+}
--- app/Libraries/helpers.php
+++ app/Libraries/helpers.php
@@ -12,3 +12,15 @@ if (! function_exists('d')) {
         return html_entity_decode($value, ENT_QUOTES, 'UTF-8');
     }
 }
+
+if (! function_exists('file_build_path')) {
+    /**
+     * Get the path according to os.
+     *
+     * @return string
+     */
+    function file_build_path()
+    {
+        return implode(DIRECTORY_SEPARATOR, func_get_args());
+    }
+}
