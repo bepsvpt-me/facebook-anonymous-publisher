8f2d855ce496dbe8ff217ad4170b5b252959f82c
BePsvPT <freedom@csie.io>
2016-05-24 19:10:28 +0800

    Ad dashboard link to navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -14,13 +14,27 @@
     <div class="collapse navbar-collapse" id="navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">排行榜 <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <span><i class="fa fa-line-chart fa-fw" aria-hidden="true"></i> 排行榜 </span><span class="caret"></span>
+          </a>
           <ul class="dropdown-menu">
             <li><a href="{{ route('ranking.daily') }}">日排行</a></li>
             <li><a href="{{ route('ranking.weekly') }}">週排行</a></li>
             <li><a href="{{ route('ranking.monthly') }}">月排行</a></li>
           </ul>
         </li>
+
+        @if(Auth::check())
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              <span><i class="fa fa-tachometer fa-fw" aria-hidden="true"></i> Dashboard </span><span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="{{ route('dashboard.posts.index') }}">文章列表</a></li>
+              <li><a href="{{ route('dashboard.block-words.index') }}">關鍵字封鎖</a></li>
+            </ul>
+          </li>
+        @endif
       </ul>
     </div>
   </div>
